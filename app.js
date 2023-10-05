9259d5f0c159f77f086d5182c3be38f8f1d82658
JTuoriniemi <119749809+JTuoriniemi@users.noreply.github.com>
2023-10-05 13:13:53 +0300

    deleted meaning eta, changed the string in app.js

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const handleRequest = async (request) => {
     data.count++;
     return new Response(await renderFile("count.eta", data), responseDetails);
   } else if (url.pathname === "/meaning") {
-    return new Response(await renderFile("meaning.eta", data), responseDetails);
+    return new Response(
+      "Seeking truths beyond meaning of life, you will find 43."
+    );
   }
 
   return new Response("Nothing here yet.");
